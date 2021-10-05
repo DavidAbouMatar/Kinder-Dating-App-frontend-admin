e76fd3e0faeb28c6a3d83a1c1274587fa0f00ea6
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-05 12:36:34 +0300

    Created Sign In Page

---
+++ src/Pages/SignIn.js
@@ -0,0 +1,93 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    let email = data.get("email");
+    let password = data.get("password");
+    try {
+      let response = await axios.post("http://127.0.0.1:8000/api/login", {
+        email,
+        password,
+      });
+      if (response.status === 200) {
+        console.log("You are logged in!");
+      } else {
+        console.log("Something went wrong!");
+      }
+    } catch (err) {
+      if (err.response.status === 401) {
+        return console.log("Wrong Credentials!");
+      }
+      return console.log(err);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+          <Typography component="h1" variant="h5">
+            Admin Log In
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
