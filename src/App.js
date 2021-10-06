6d1f08cd866a5db4d33cca948514afafde47794a
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-06 09:21:22 +0300

    Added SignIn page to the Navigator

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route exact path="/home">
           <Home />
         </Route>
         <Route exact path="/pendingimgs">
